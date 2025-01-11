e5913052107c410640e9d5cda38e36dfe34947ad
Billiam <billiamthesecond@gmail.com>
2025-01-11 15:48:27 -0600

    Add (very basic) log level filtering

--- crashlog_dialog.lua
+++ crashlog_dialog.lua
@@ -7,18 +7,35 @@ local UIManager = require("ui/uimanager")
 
 local ScrollTextWidget = require("ui/widget/scrolltextwidget")
 local TitleBar = require("ui/widget/titlebar")
+local ToggleSwitch = require("ui/widget/toggleswitch")
 local VerticalGroup = require("ui/widget/verticalgroup")
 
 local FrameContainer = require("ui/widget/container/framecontainer")
 local RightContainer = require("ui/widget/container/rightcontainer")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
-
 local CrashlogDialog = WidgetContainer:extend {
   width = Screen:getWidth(),
   height = Screen:getHeight(),
   padding = Screen:scaleBySize(5),
   title = nil,
-  text = nil
+  text = nil,
+  log_level = 1
+}
+
+local LOG_LEVELS = {
+  "ALL",
+  "DEBUG",
+  "INFO",
+  "WARN",
+  "ERROR"
+}
+
+local LOG_LEVEL_INDEX = {
+  ALL = 1,
+  DEBUG = 2,
+  INFO = 3,
+  WARN = 4,
+  ERROR = 5,
 }
 
 function CrashlogDialog:init()
@@ -27,23 +44,30 @@ function CrashlogDialog:init()
     with_bottom_line = true,
     left_icon = "cre.render.reload",
     left_icon_tap_callback = function()
-      local new_text = self.refresh_func()
-      local old_height = self.text_container.height
-      self.text_container:free()
-
-      self.text_container = self:buildTextContainer(new_text, old_height)
-      self.container_parent[1] = self.text_container
-      UIManager:nextTick(function()
-        UIManager:setDirty(self, "ui", self.text_container.dimen)
-      end)
+      local new_text = self:getText()
+      self:refreshContainer(new_text)
     end,
     close_callback = function()
       UIManager:close(self)
     end
   }
   local titlebar_size = titlebar:getSize()
-  local text_container_height = self.height - titlebar_size.h
-  self.text_container = self:buildTextContainer(self.text, text_container_height)
+
+  local filter = ToggleSwitch:new {
+    width = self.width,
+    toggle = {
+      "All", "Debug", "Info", "Warn", "Error"
+    },
+    values = LOG_LEVELS,
+    config = self,
+    alternate = false,
+  }
+  filter:setPosition(1)
+
+  local filter_size = filter:getSize()
+
+  local text_container_height = self.height - titlebar_size.h - filter_size.h
+  self.text_container = self:buildTextContainer(self:filterText(self.text), text_container_height)
 
   self.container_parent = RightContainer:new {
     dimen = Geom:new {
@@ -62,11 +86,56 @@ function CrashlogDialog:init()
     VerticalGroup:new {
       align = "left",
       titlebar,
+      filter,
       self.container_parent,
     }
   }
 
   self[1] = frame
+end
+
+function CrashlogDialog:getText()
+  self.text = self.refresh_func()
+  return self:filterText(self.text)
+end
+
+function CrashlogDialog:filterText(text)
+  if self.log_level == 1 then
+    return text
+  end
+
+  local output_t = {}
+  local i = 1
+  for line in text:gmatch("[^\n]*\n?") do
+    local level = line:match("^[%d-/:]+ ([A-Z]+)")
+    if LOG_LEVEL_INDEX[level] and LOG_LEVEL_INDEX[level] >= self.log_level then
+      output_t[i] = line
+      i = i + 1
+    end
+  end
+
+  return table.concat(output_t, "")
+end
+
+function CrashlogDialog:onConfigChoose(_values, _name, _event, _args, position)
+  if self.log_level ~= position then
+    self.log_level = position
+  end
+
+  local filtered_text = self:filterText(self.text)
+  self:refreshContainer(filtered_text)
+end
+
+function CrashlogDialog:refreshContainer(text)
+  local old_height = self.text_container.height
+  self.text_container:free()
+
+  self.text_container = self:buildTextContainer(text, old_height)
+  self.container_parent[1] = self.text_container
+
+  UIManager:nextTick(function()
+    UIManager:setDirty(self, "ui", self.text_container.dimen)
+  end)
 end
 
 function CrashlogDialog:buildTextContainer(text, height)
